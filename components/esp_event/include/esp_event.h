52a9e9b63d2c2d557dce7c937a91d6ac5744a427
ebisuke <fungi@rect.sakura.ne.jp>
2023-01-06 23:40:00 +0900

    Update to latest upstream

--- components/esp_event/include/esp_event.h
+++ components/esp_event/include/esp_event.h
@@ -521,8 +521,8 @@ esp_err_t esp_event_isr_post_to(esp_event_loop_handle_t event_loop,
  */
 esp_err_t esp_event_dump(FILE *file);
 
-esp_event_handler_t esp_event_get_default_handler(void);
-void esp_event_set_default_handler(esp_event_handler_t handler);
+esp_event_loop_handle_t esp_event_get_default_loop_handle(void);
+void esp_event_set_default_loop_handle(esp_event_loop_handle_t handler);
 
 #ifdef __cplusplus
 } // extern "C"
